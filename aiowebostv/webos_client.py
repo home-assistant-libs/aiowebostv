8fc33be838a92bce599715eb155685a1c19664c5
Shay Levy <levyshay1@gmail.com>
2026-08-19 22:07:25 +0300

    Fix webOS 26 pairing handshake

--- aiowebostv/webos_client.py
+++ aiowebostv/webos_client.py
@@ -268,7 +268,10 @@ class WebOsClient:
             with suppress(WebOsTvResponseTypeError):
                 self.tv_info.system = await self.get_system_info()
 
-        self.tv_info.software = await self.get_software_info()
+        # Try to get software info; will fail if TV was paired with legacy handshake
+        with suppress(WebOsTvResponseTypeError):
+            self.tv_info.software = await self.get_software_info()
+
         with suppress(WebOsTvServiceNotFoundError):
             self.tv_info.connection = await self.get_connection_info()
 
